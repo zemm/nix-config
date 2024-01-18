55c0f5647d30c12fab88c372258748439d758e27
Jussi Rajala <zemm@iki.fi>
2024-01-18 14:15:27 +0200

    Refactor Sway deep nests to less deep

--- home/parts/sway/sway.nix
+++ home/parts/sway/sway.nix
@@ -163,147 +163,154 @@
   in {
     enable = true;
 
-    config = rec {
-      # https://man.archlinux.org/man/sway-input.5
-      input = {
-        # Dell latitude
-        "1:1:AT_Translated_Set_2_keyboard" = {
-          xkb_layout = "us";
-          xkb_variant = "colemak";
-          xkb_options = "caps:backspace,nbsp:none";
-        };
-        "type:keyboard" = {
-          repeat_rate = "36";
-          repeat_delay = "220";
-        };
-        "type:touchpad" = {
-          natural_scroll = "enabled";
-          dwt = "enabled";
-          #tap = "enabled";
-          click_method = "clickfinger";
-          accel_profile = "adaptive";
-          pointer_accel = "0.3";
-        };
+    # https://man.archlinux.org/man/sway-input.5
+    config.input = {
+      # Dell latitude
+      "1:1:AT_Translated_Set_2_keyboard" = {
+        xkb_layout = "us";
+        xkb_variant = "colemak";
+        xkb_options = "caps:backspace,nbsp:none";
       };
-
-      menu = "${pkgs.rofi}/bin/rofi -show drun";
-
-      # https://nix-community.github.io/home-manager/options.xhtml#opt-wayland.windowManager.sway.config.keybindings
-      keybindings = {
-        "${modifier}+Return" = "exec ${cfg.config.terminal}";
-        "${modifier}+Shift+q" = "kill";
-        #"${modifier}+d" = "exec ${cfg.config.menu}";
-        "${modifier}+Space" = "exec ${cfg.config.menu}";
-
-        #"${modifier}+${cfg.config.left}" = "focus left";
-        #"${modifier}+${cfg.config.down}" = "focus down";
-        #"${modifier}+${cfg.config.up}" = "focus up";
-        #"${modifier}+${cfg.config.right}" = "focus right";
-
-        "${modifier}+Left" = "focus left";
-        "${modifier}+Down" = "focus down";
-        "${modifier}+Up" = "focus up";
-        "${modifier}+Right" = "focus right";
-
-        # Move focused container
-        "${modifier}+Shift+${cfg.config.left}" = "move left";
-        "${modifier}+Shift+${cfg.config.down}" = "move down";
-        "${modifier}+Shift+${cfg.config.up}" = "move up";
-        "${modifier}+Shift+${cfg.config.right}" = "move right";
-
-        "${modifier}+Shift+Left" = "move left";
-        "${modifier}+Shift+Down" = "move down";
-        "${modifier}+Shift+Up" = "move up";
-        "${modifier}+Shift+Right" = "move right";
-
-        # Move focused workspace to another monitor
-        "${modifier}+Control+Left" = "move workspace to output left";
-        "${modifier}+Control+Right" = "move workspace to output right";
-        "${modifier}+Control+Up" = "move workspace to output up";
-        "${modifier}+Control+Down" = "move workspace to output down";
-
-        "${modifier}+h" = "splith";
-        "${modifier}+v" = "splitv";
-        "${modifier}+f" = "fullscreen toggle";
-        "${modifier}+p" = "focus parent";
-        "${modifier}+Shift+p" = "focus child";
-
-        "${modifier}+s" = "layout stacking";
-        "${modifier}+w" = "layout tabbed";
-        "${modifier}+t" = "layout toggle split";
-
-        "${modifier}+Shift+f" = "floating toggle";
-        "${modifier}+a" = "focus mode_toggle";
-
-        "${modifier}+1" = "workspace number 1";
-        "${modifier}+2" = "workspace number 2";
-        "${modifier}+3" = "workspace number 3";
-        "${modifier}+4" = "workspace number 4";
-        "${modifier}+5" = "workspace number 5";
-        "${modifier}+6" = "workspace number 6";
-        "${modifier}+7" = "workspace number 7";
-        "${modifier}+8" = "workspace number 8";
-        "${modifier}+9" = "workspace number 9";
-
-        "${modifier}+Shift+1" = "move container to workspace number 1";
-        "${modifier}+Shift+2" = "move container to workspace number 2";
-        "${modifier}+Shift+3" = "move container to workspace number 3";
-        "${modifier}+Shift+4" = "move container to workspace number 4";
-        "${modifier}+Shift+5" = "move container to workspace number 5";
-        "${modifier}+Shift+6" = "move container to workspace number 6";
-        "${modifier}+Shift+7" = "move container to workspace number 7";
-        "${modifier}+Shift+8" = "move container to workspace number 8";
-        "${modifier}+Shift+9" = "move container to workspace number 9";
-
-        "${modifier}+Shift+minus" = "move scratchpad";
-        "${modifier}+minus" = "scratchpad show";
-
-        "${modifier}+Shift+r" = "reload";
-        "${modifier}+Shift+x" =
-          "exec swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -b 'Yes, exit sway' 'swaymsg exit'";
-
-        "${modifier}+r" = "mode resize";
-
-        "${modifier}+End" = "exec ${pkgs.swaylock}/bin/swaylock -f && ${pkgs.systemd}/bin/systemctl suspend";
-        "${modifier}+Home" = "exec ${pkgs.swaylock}/bin/swaylock";
-        "${modifier}+c" = "exec rofi -show calc";
+      "type:keyboard" = {
+        repeat_rate = "36";
+        repeat_delay = "220";
       };
-
-      # home row keys for moving
-      # @TODO: fix collision on mod4+e (do colemak remapping)
-      #left = "n";
-      #right = "i";
-      #up = "u";
-      #down = "e";
-
-      #menu = ""; https://nix-community.github.io/home-manager/options.xhtml#opt-wayland.windowManager.sway.config.menu
-      #modes = {}; https://nix-community.github.io/home-manager/options.xhtml#opt-wayland.windowManager.sway.config.modes
-
-      modifier = "Mod4";
-
-      #outputs = {}; https://nix-community.github.io/home-manager/options.xhtml#opt-wayland.windowManager.sway.config.outputs
-      #seat = {}; # https://nix-community.github.io/home-manager/options.xhtml#opt-wayland.windowManager.sway.config.seat
-
-      # https://nix-community.github.io/home-manager/options.xhtml#opt-wayland.windowManager.sway.config.startup
-      startup = [
-        { command = "wpaperd"; }
-        { command = "nm-applet"; }
-        { command = "blueman-applet"; }
-        #{ command = "volumeicon"; } # does not work
-        { command = "systemctl --user restart waybar || :";
-          always = true;
-        }
-        { command = "systemctl --user restart kanshi || :";
-          always = true;
-        }
-      ];
-
-      terminal = "alacritty";
-
-      workspaceAutoBackAndForth = true;
-
-      #workspaceOutputAssign = {}; # https://nix-community.github.io/home-manager/options.xhtml#opt-wayland.windowManager.sway.config.workspaceOutputAssign
+      "type:touchpad" = {
+        natural_scroll = "enabled";
+        dwt = "enabled";
+        #tap = "enabled";
+        click_method = "clickfinger";
+        accel_profile = "adaptive";
+        pointer_accel = "0.3";
+      };
     };
+
+    config.menu = "${pkgs.rofi}/bin/rofi -show drun";
+
+    # https://nix-community.github.io/home-manager/options.xhtml#opt-wayland.windowManager.sway.config.keybindings
+    config.keybindings = let
+      modifier = cfg.config.modifier;
+    in {
+      "${modifier}+Return" = "exec ${cfg.config.terminal}";
+      "${modifier}+Shift+q" = "kill";
+      #"${modifier}+d" = "exec ${cfg.config.menu}";
+      "${modifier}+Space" = "exec ${cfg.config.menu}";
+
+      #"${modifier}+${cfg.config.left}" = "focus left";
+      #"${modifier}+${cfg.config.down}" = "focus down";
+      #"${modifier}+${cfg.config.up}" = "focus up";
+      #"${modifier}+${cfg.config.right}" = "focus right";
+
+      "${modifier}+Left" = "focus left";
+      "${modifier}+Down" = "focus down";
+      "${modifier}+Up" = "focus up";
+      "${modifier}+Right" = "focus right";
+
+      # Move focused container
+      "${modifier}+Shift+${cfg.config.left}" = "move left";
+      "${modifier}+Shift+${cfg.config.down}" = "move down";
+      "${modifier}+Shift+${cfg.config.up}" = "move up";
+      "${modifier}+Shift+${cfg.config.right}" = "move right";
+
+      "${modifier}+Shift+Left" = "move left";
+      "${modifier}+Shift+Down" = "move down";
+      "${modifier}+Shift+Up" = "move up";
+      "${modifier}+Shift+Right" = "move right";
+
+      # Move focused workspace to another monitor
+      "${modifier}+Control+Left" = "move workspace to output left";
+      "${modifier}+Control+Right" = "move workspace to output right";
+      "${modifier}+Control+Up" = "move workspace to output up";
+      "${modifier}+Control+Down" = "move workspace to output down";
+
+      "${modifier}+h" = "splith";
+      "${modifier}+v" = "splitv";
+      "${modifier}+f" = "fullscreen toggle";
+      "${modifier}+p" = "focus parent";
+      "${modifier}+Shift+p" = "focus child";
+
+      "${modifier}+s" = "layout stacking";
+      "${modifier}+w" = "layout tabbed";
+      "${modifier}+t" = "layout toggle split";
+
+      "${modifier}+Shift+f" = "floating toggle";
+      "${modifier}+a" = "focus mode_toggle";
+
+      "${modifier}+1" = "workspace number 1";
+      "${modifier}+2" = "workspace number 2";
+      "${modifier}+3" = "workspace number 3";
+      "${modifier}+4" = "workspace number 4";
+      "${modifier}+5" = "workspace number 5";
+      "${modifier}+6" = "workspace number 6";
+      "${modifier}+7" = "workspace number 7";
+      "${modifier}+8" = "workspace number 8";
+      "${modifier}+9" = "workspace number 9";
+
+      "${modifier}+Shift+1" = "move container to workspace number 1";
+      "${modifier}+Shift+2" = "move container to workspace number 2";
+      "${modifier}+Shift+3" = "move container to workspace number 3";
+      "${modifier}+Shift+4" = "move container to workspace number 4";
+      "${modifier}+Shift+5" = "move container to workspace number 5";
+      "${modifier}+Shift+6" = "move container to workspace number 6";
+      "${modifier}+Shift+7" = "move container to workspace number 7";
+      "${modifier}+Shift+8" = "move container to workspace number 8";
+      "${modifier}+Shift+9" = "move container to workspace number 9";
+
+      "${modifier}+Shift+minus" = "move scratchpad";
+      "${modifier}+minus" = "scratchpad show";
+
+      "${modifier}+Shift+r" = "reload";
+      "${modifier}+Shift+x" =
+        "exec swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -b 'Yes, exit sway' 'swaymsg exit'";
+
+      "${modifier}+r" = "mode resize";
+
+      "${modifier}+End" = "exec ${pkgs.swaylock}/bin/swaylock -f && ${pkgs.systemd}/bin/systemctl suspend";
+      "${modifier}+Home" = "exec ${pkgs.swaylock}/bin/swaylock";
+      "${modifier}+c" = "exec rofi -show calc";
+    };
+
+    # home row keys for moving
+    # @TODO: fix collision on mod4+e (do colemak remapping)
+    #config.left = "n";
+    #config.right = "i";
+    #config.up = "u";
+    #config.down = "e";
+
+    # https://nix-community.github.io/home-manager/options.xhtml#opt-wayland.windowManager.sway.config.menu
+    #config.menu = "";
+
+    # https://nix-community.github.io/home-manager/options.xhtml#opt-wayland.windowManager.sway.config.modes
+    #config.modes = {};
+
+    config.modifier = "Mod4";
+
+    # https://nix-community.github.io/home-manager/options.xhtml#opt-wayland.windowManager.sway.config.outputs
+    #config.outputs = {};
+
+    # https://nix-community.github.io/home-manager/options.xhtml#opt-wayland.windowManager.sway.config.seat
+    #config.seat = {};
+
+    # https://nix-community.github.io/home-manager/options.xhtml#opt-wayland.windowManager.sway.config.startup
+    config.startup = [
+      { command = "wpaperd"; }
+      { command = "nm-applet"; }
+      { command = "blueman-applet"; }
+      #{ command = "volumeicon"; } # does not work
+      { command = "systemctl --user restart waybar || :";
+        always = true;
+      }
+      { command = "systemctl --user restart kanshi || :";
+        always = true;
+      }
+    ];
+
+    config.terminal = "alacritty";
+
+    config.workspaceAutoBackAndForth = true;
+
+    # https://nix-community.github.io/home-manager/options.xhtml#opt-wayland.windowManager.sway.config.workspaceOutputAssign
+    #config.workspaceOutputAssign = {};
 
     config.window.commands = [
       { criteria = { app_id = "org.gnome.Calculator"; };
