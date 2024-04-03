51097c15af90f59f1a34b1542959d223aa687dab
Jussi Rajala <zemm@iki.fi>
2024-04-03 23:21:03 +0300

    Stoat: disable predefined display profiles for now

--- home/stoat.nix
+++ home/stoat.nix
@@ -16,49 +16,49 @@
 
   manual.html.enable = true;
 
-  services.kanshi = {
-    enable = true;
-    # Use: swaymsg -t get_outputs
-    profiles = {
-      int = {
-        outputs = [
-          {
-            criteria = "eDP-1";
-            mode = "1920x1200";
-            position = "0,0";
-          }
-        ];
-      };
-      dell_4k_top = {
-        outputs = [
-          {
-            criteria = "Dell Inc. DELL P3222QE 3WJGT83";
-            mode = "3840x2160";
-            position = "0,0";
-          }
-          {
-            criteria = "eDP-1";
-            mode = "1920x1200";
-            position = "960,2160";
-          }
-        ];
-      };
-      aoc24_top = {
-        outputs = [
-          {
-            criteria = "AOC 24G2W1G4 0x000054B4";
-            mode = "1920x1080";
-            position = "0,0";
-          }
-          {
-            criteria = "eDP-1";
-            mode = "1920x1200";
-            position = "0,1080";
-          }
-        ];
-      };
-    };
-  };
+  #services.kanshi = {
+  #  enable = true;
+  #  # Use: swaymsg -t get_outputs
+  #  profiles = {
+  #    int = {
+  #      outputs = [
+  #        {
+  #          criteria = "eDP-1";
+  #          mode = "1920x1200";
+  #          position = "0,0";
+  #        }
+  #      ];
+  #    };
+  #    dell_4k_top = {
+  #      outputs = [
+  #        {
+  #          criteria = "Dell Inc. DELL P3222QE 3WJGT83";
+  #          mode = "3840x2160";
+  #          position = "0,0";
+  #        }
+  #        {
+  #          criteria = "eDP-1";
+  #          mode = "1920x1200";
+  #          position = "960,2160";
+  #        }
+  #      ];
+  #    };
+  #    aoc24_top = {
+  #      outputs = [
+  #        {
+  #          criteria = "AOC 24G2W1G4 0x000054B4";
+  #          mode = "1920x1080";
+  #          position = "0,0";
+  #        }
+  #        {
+  #          criteria = "eDP-1";
+  #          mode = "1920x1200";
+  #          position = "0,1080";
+  #        }
+  #      ];
+  #    };
+  #  };
+  #};
 
   # @TODO: programs.borgmatic
   # @TODO: services.borgmatic
